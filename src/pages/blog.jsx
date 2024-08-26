eccee9e819d504f83ccca922ed5b66c7955f0c97
ysshin_dw <ysshin_dw@amorepacific.com>
2024-08-26 16:20:13 +0900

    템플릿 코드 정리 (2)

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -8,7 +8,7 @@ export default function BlogPage() {
   return (
     <>
       <Helmet>
-        <title> Blog | Minimal UI </title>
+        <title> 운사 </title>
       </Helmet>
 
       <BlogView />
